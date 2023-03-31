0cb80d67eee6bf912e0a91a05d686e03d5106db6
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-03-31 20:52:44 +0200

    update hw-07-js-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const itemImage = document.querySelector('.gallery');
+
+const markupGallery = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+<a class="gallery__link" href= "${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  /></a></li>`
+}).join('');
+
+itemImage.insertAdjacentHTML('beforeend', markupGallery);
+
+itemImage.addEventListener('click', onModal);
+
+function onModal(evt) {
+    evt.preventDefault();
+
+    const currentImage = evt.target;
+
+    if (currentImage.nodeName !== 'IMG') {
+        return;
+    }
+    let instance = basicLightbox.create(`<img class="gallery__image"
+    src="${currentImage.dataset.source}"/>`)
+
+    instance.show();
+
+    // console.dir(currentImage.nodeName);
+}
+
+
+
+// console.log(galleryItems);
